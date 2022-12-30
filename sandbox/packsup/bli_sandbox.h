64bb20236269ccef91281d1d0fdb8f2dd1a62c7e
RuQing Xu <ruqing.xu@phys.s.u-tokyo.ac.jp>
2022-12-31 02:04:06 +0900

    packsup x86 Cleanup Uker Solitons
    Except for N-Edges.

--- sandbox/packsup/bli_sandbox.h
+++ sandbox/packsup/bli_sandbox.h
@@ -5,110 +5,37 @@
 extern "C" {
 #endif
 
-void bli_dgemmsup2_rv_haswell_asm_6x8r
-    (
-     dim_t            m,
-     dim_t            n,
-     dim_t            k,
-     double *restrict alpha,
-     double *restrict a, inc_t rs_a0, inc_t cs_a0,
-     double *restrict b, inc_t rs_b0, inc_t cs_b0,
-     double *restrict beta,
-     double *restrict c, inc_t rs_c0, inc_t cs_c0,
-     auxinfo_t       *data,
-     cntx_t          *cntx,
-     double *restrict a_p, int pack_a,
-     double *restrict b_p, int pack_b
-    );
-
-void bli_dgemmsup2_rv_haswell_asm_6x8m
-    (
-     dim_t            m,
-     dim_t            n,
-     dim_t            k,
-     double *restrict alpha,
-     double *restrict a, inc_t rs_a0, inc_t cs_a0,
-     double *restrict b, inc_t rs_b0, inc_t cs_b0,
-     double *restrict beta,
-     double *restrict c, inc_t rs_c0, inc_t cs_c0,
-     auxinfo_t       *data,
-     cntx_t          *cntx,
-     double *restrict a_p, int pack_a,
-     double *restrict b_p, int pack_b
-    );
-
-void bli_dgemmsup2_rv_armv8a_asm_8x6r
-    (
-     dim_t            m,
-     dim_t            n,
-     dim_t            k,
-     double *restrict alpha,
-     double *restrict a, inc_t rs_a0, inc_t cs_a0,
-     double *restrict b, inc_t rs_b0, inc_t cs_b0,
-     double *restrict beta,
-     double *restrict c, inc_t rs_c0, inc_t cs_c0,
-     auxinfo_t       *data,
-     cntx_t          *cntx,
-     double *restrict a_p, int pack_a,
-     double *restrict b_p, int pack_b
-    );
-
-void bli_dgemmsup2_rv_armv8a_asm_8x6c
-    (
-     dim_t            m,
-     dim_t            n,
-     dim_t            k,
-     double *restrict alpha,
-     double *restrict a, inc_t rs_a0, inc_t cs_a0,
-     double *restrict b, inc_t rs_b0, inc_t cs_b0,
-     double *restrict beta,
-     double *restrict c, inc_t rs_c0, inc_t cs_c0,
-     auxinfo_t       *data,
-     cntx_t          *cntx,
-     double *restrict a_p, int pack_a,
-     double *restrict b_p, int pack_b
-    );
-
-void bli_dgemmsup2_cv_armv8a_asm_8x6r
-    (
-     dim_t            m,
-     dim_t            n,
-     dim_t            k,
-     double *restrict alpha,
-     double *restrict a, inc_t rs_a0, inc_t cs_a0,
-     double *restrict b, inc_t rs_b0, inc_t cs_b0,
-     double *restrict beta,
-     double *restrict c, inc_t rs_c0, inc_t cs_c0,
-     auxinfo_t       *data,
-     cntx_t          *cntx,
-     double *restrict a_p, int pack_a,
-     double *restrict b_p, int pack_b
-    );
-
-void bli_dgemmsup2_cv_armv8a_asm_8x6c
-    (
-     dim_t            m,
-     dim_t            n,
-     dim_t            k,
-     double *restrict alpha,
-     double *restrict a, inc_t rs_a0, inc_t cs_a0,
-     double *restrict b, inc_t rs_b0, inc_t cs_b0,
-     double *restrict beta,
-     double *restrict c, inc_t rs_c0, inc_t cs_c0,
-     auxinfo_t       *data,
-     cntx_t          *cntx,
-     double *restrict a_p, int pack_a,
-     double *restrict b_p, int pack_b
-    );
+#define SUPKER_DECL(funcname) \
+void funcname \
+    ( \
+     dim_t            m, \
+     dim_t            n, \
+     dim_t            k, \
+     double *restrict alpha, \
+     double *restrict a, inc_t rs_a0, inc_t cs_a0, \
+     double *restrict b, inc_t rs_b0, inc_t cs_b0, \
+     double *restrict beta, \
+     double *restrict c, inc_t rs_c0, inc_t cs_c0, \
+     auxinfo_t       *data, \
+     cntx_t          *cntx, \
+     double *restrict a_p, int pack_a, \
+     double *restrict b_p, int pack_b \
+    )
 
 #if defined(__x86_64__) || defined(_M_X64) || defined(__i386) || defined(_M_IX86)
 
+SUPKER_DECL(bli_dgemmsup2_rv_armv8a_asm_8x6r);
+SUPKER_DECL(bli_dgemmsup2_rv_armv8a_asm_8x6c);
+SUPKER_DECL(bli_dgemmsup2_cv_armv8a_asm_8x6r);
+SUPKER_DECL(bli_dgemmsup2_cv_armv8a_asm_8x6c);
 typedef typeof(&bli_dpackm_haswell_asm_8xk) l1mukr_t;
 typedef typeof(&bli_dgemm_haswell_asm_6x8) ukr_dgemm_bulk_t;
 typedef typeof(&bli_dgemmsup2_rv_haswell_asm_6x8r) ukr_dgemm_sup_t;
 
 #elif defined(__aarch64__) || defined(__arm__) || defined(_M_ARM) || defined(_ARCH_PPC)
 
+SUPKER_DECL(bli_dgemmsup2_rv_haswell_asm_6x8m);
+SUPKER_DECL(bli_dgemmsup2_rv_haswell_asm_6x8rn);
 typedef typeof(&bli_dpackm_armv8a_int_8xk) l1mukr_t;
 typedef typeof(&bli_dgemm_armv8a_asm_8x6r) ukr_dgemm_bulk_t;
 typedef typeof(&bli_dgemmsup2_rv_armv8a_asm_8x6r) ukr_dgemm_sup_t;
